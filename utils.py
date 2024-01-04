5060d5bf8afb84e0abf1e86710372d953bb6863c
Emanuele Sansone <e.sansone@hotmail.it>
2024-01-04 07:53:25 +0100

    fixed typo

--- utils.py
+++ utils.py
@@ -470,10 +470,13 @@ def init_distributed_mode(args):
         args.rank = int(os.environ["RANK"])
         args.world_size = int(os.environ['WORLD_SIZE'])
         args.gpu = int(os.environ['LOCAL_RANK'])
-    # launched with submitit on a slurm cluster
-    elif 'SLURM_PROCID' in os.environ:
-        args.rank = int(os.environ['SLURM_PROCID'])
-        args.gpu = args.rank % torch.cuda.device_count()
+
+    # ------- COMMENTED OUT IN ORDER NOT TO MESS UP WITH SLURM ----
+    # # launched with submitit on a slurm cluster
+    # elif 'SLURM_PROCID' in os.environ:
+    #     args.rank = int(os.environ['SLURM_PROCID'])
+    #     args.gpu = args.rank % torch.cuda.device_count()
+
     # launched naively with `python main_dino.py`
     # we manually add MASTER_ADDR and MASTER_PORT to env variables
     elif torch.cuda.is_available():
